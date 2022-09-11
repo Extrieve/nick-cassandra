33cfb956ddf036d4b0a710648074df80170d5a57
Nick <nicolas.cinera@gmail.com>
2022-09-11 18:46:59 -0400

    Setting up transaction test scenarios

--- src/test/java/com/nickcassandra/service/StudentDelegatorTest.java
+++ src/test/java/com/nickcassandra/service/StudentDelegatorTest.java
@@ -1,9 +1,12 @@
 package com.nickcassandra.service;
 
 import com.nickcassandra.entity.Student;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,6 +15,19 @@ class StudentDelegatorTest {
 
     @Autowired
     private StudentDelegator studentDelegator;
+
+    @BeforeEach
+    void setUp() {
+
+        Student student = Student
+                .builder()
+                .studentId(2L)
+                .email("nicolas.cinera@gmail.com")
+                .firstName("Nicolas")
+                .lastName("Cinera")
+                .build();
+
+    }
 
     @Test
     public void createStudent() {
@@ -27,6 +43,27 @@ class StudentDelegatorTest {
         Student fetchStudent = studentDelegator.getStudent(1L).getBody();
         assertEquals(student.getEmail(), fetchStudent.getEmail());
     }
+    
+    @Test
+    public void updateStudent() {
+        String newEmail = "cineran@usf.edu";
+        Student student = Student
+                .builder()
+                .studentId(2L)
+                .email(newEmail)
+                .firstName("Nick")
+                .lastName("Cinera")
+                .build();
 
+        studentDelegator.updateStudent(student);
+        Student fetchStudent = studentDelegator.getStudent(2L).getBody();
+        assertEquals(newEmail, Objects.requireNonNull(fetchStudent).getEmail());
+    }
 
+    @Test
+    public void deleteStudent() {
+        studentDelegator.deleteStudent(2L);
+        Student fetchStudent = studentDelegator.getStudent(2L).getBody();
+        assertNull(fetchStudent);
+    }
 }
